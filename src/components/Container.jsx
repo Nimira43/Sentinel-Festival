e0d90c4493ef3cfd953ca66c02ac0681b0977695
Nimira43 <n.rabone72@aol.com>
2024-12-07 18:38:25 +0000

    Setting up initisl components

---
+++ src/components/Container.jsx
@@ -0,0 +1,12 @@
+import Canvas from './Canvas' 
+import UI from './UI' 
+
+function Container() { 
+  return ( 
+    <div id='container'> 
+      <Canvas /> 
+      <UI /> 
+    </div> 
+  ) 
+} 
+export default Container
