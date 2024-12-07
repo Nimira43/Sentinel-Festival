118682b080c216eacfa06ed7bea6a1dc87836a94
Nimira43 <n.rabone72@aol.com>
2024-12-07 18:53:36 +0000

    Setup UI component

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -1,8 +1,21 @@
-function UI() { 
-  return ( 
-    <div>
-      <h1>UI</h1>
-    </div> 
-  ) 
-} 
+import LevelSelect from './LevelSelect'
+import Intro from './Intro'
+import End from './End'
+import LevelTitle from './LevelTitle'
+import IconsTop from './IconsTop'
+import Footer from './Footer'
+
+function UI() {
+  return (
+    <div id="ui">
+      <LevelSelect />
+      <Intro />
+      <End />
+      <LevelTitle />
+      <IconsTop />
+      <Footer />
+    </div>
+  )
+}
+
 export default UI
