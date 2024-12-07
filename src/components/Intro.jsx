6dcc2aa8d6c6d5c61bfe092122451e8469398171
Nimira43 <n.rabone72@aol.com>
2024-12-07 20:14:47 +0000

    Developed intro scene.

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,8 +1,19 @@
-function Intro() { 
-  return ( 
-    <div>
-      <h1>Intro</h1>
-    </div> 
-  ) 
-} 
-export default Intro
+import cubeImage from '../assets/images/cube.png';
+
+function Intro() {
+  return (
+    <div id="intro">
+      <div id="title">
+        Transcube<span style={{ backgroundImage: `url(${cubeImage})` }}>React</span>
+      </div>
+      <div id="slug">
+        <p>a game by <a href="https://github.com/jeroenverfallie">jeroen verfallie</a></p>
+        <p>adapted by <a href='https://github.com/Nimira43'>nimiratech</a></p>
+      </div>
+      <div id="play" className="button">Play</div>
+      <div id="github" className="button">Github</div>
+    </div>
+  );
+}
+
+export default Intro;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import less from 'vite-plugin-less'
 
 export default defineConfig({
-  plugins: [react(), less()],
+  plugins: [react()],
 })
