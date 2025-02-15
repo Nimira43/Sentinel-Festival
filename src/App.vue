8ee782f68631b84c050c2d4078cafdccc011f766
Nimira43 <n.rabone72@aol.com>
2025-02-15 06:16:22 +0000

    Working on Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,5 @@
 <template>
   <NavBar />
-  <RouterLink 
-    to="/" 
-    class="button text-light bg-prime"
-  >
-    Notes
-  </RouterLink>
-  <RouterLink 
-    to="/stats" 
-    class="button text-light bg-dark" 
-  >
-    Stats
-  </RouterLink>
-  <RouterView />
 </template>
 
 <script setup>
--- src/components/Layout/NavBar.vue
+++ src/components/Layout/NavBar.vue
@@ -1,3 +1,26 @@
 <template>
-  NavBar
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <h1 class="logo">Sentinel</h1>
+      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button text-light bg-prime">
+              <strong>Notes</strong>
+            </a>
+            <a class="button text-light bg-dark">
+              <strong>Stats</strong>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
 </template>
