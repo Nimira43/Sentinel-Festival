d3f0dc9c9f36e93b393560f97e86594339622b65
Nimira43 <n.rabone72@aol.com>
2025-02-15 05:52:11 +0000

    Adding custom styling to the Bulma button

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
-  <RouterLink to="/">Notes</RouterLink>
-  <RouterLink to="/stats" >Stats</RouterLink>
+  <RouterLink 
+    to="/" 
+    class="button text-light bg-prime"
+  >
+    Notes
+  </RouterLink>
+  <RouterLink 
+    to="/stats" 
+    class="button text-light bg-dark" 
+  >
+    Stats
+  </RouterLink>
   <RouterView />
 </template>
 
+<style>
+
+
+</style>
