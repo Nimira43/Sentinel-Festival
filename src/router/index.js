c84ad6a087a5b2d56fb3c8fd6ad44060bffaf92f
Nimira43 <n.rabone72@aol.com>
2025-02-15 05:19:23 +0000

    Corrected error on line 3 of index.js and inmported router into main.js.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import ViewNotes from '@/views/ViewNotes.vue'
-import ViewStats from './views/ViewStats.vue'
+import ViewStats from '@/views/ViewStats.vue'
 
 const router = createRouter({
   history: createWebHashHistory(),
