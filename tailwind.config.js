f93c0fd81a7e59c86df25e888f182a190f31ae41
Nimira43 <n.rabone72@aol.com>
2025-02-18 05:09:11 +0000

    Create index.html and setup stylesheet. Set up colour themese in tailwind.config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     './src/**/*.{html,js}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      color: {
+        'prime': '#ff4500',
+        'support': '#87ceeb',
+        'dark': '#111',
+        'light': '#fffcfa',
+        'grey-dark': '#333',
+        'grey-medium': '#999',
+        'grey-light': '#ccc',
+        'grey-light-extra': '#eee',
+      },
+    },
   },
   plugins: [],
 }
